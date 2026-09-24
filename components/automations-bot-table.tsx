9a4de25ef9698adacd89aec5a4829d47cc272c7e
NVL <317450778+ClickStudioAdmin@users.noreply.github.com>
2026-09-24 16:46:16 +1000

    T

--- components/automations-bot-table.tsx
+++ components/automations-bot-table.tsx
@@ -547,8 +547,8 @@ export function AutomationsBotTable({
                   </TableIconAction>
                   <TableIconAction
                     href={row.editHref}
-                    label="Modify the bot’s parameters & status"
-                    detail=""
+                    label="Edit"
+                    detail="Modify the bot’s parameters & status"
                   >
                     <IconPencil {...TABLE_BTN_ICON} />
                   </TableIconAction>
